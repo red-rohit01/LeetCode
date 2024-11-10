7ea3d78b5fffdd21fd036e45628e790fb3f55fd3
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2024-11-10 09:09:19 +0530

    Time: 39 ms (90.11%), Space: 88.2 MB (74.37%) - LeetHub

--- 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.cpp
+++ 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.cpp
@@ -12,23 +12,20 @@ public:
         while(j<n)
         {
             val|=(1LL*nums[j]);
-            //cout<<val<<" ";
             long long temp=1LL*nums[j];
-            for(int l=0;l<32;++l)
+            for(int l=0;l<32;++l)            // Taking the contribution of this particular element on bits
             {
                 int curr=(1&temp);
                 temp/=2;
                 if(curr>0) pre[l]++;  
             }
             
-            //cout<<pre[0]<<" "<<pre[1]<<"\n";
-            
-            while(val>=(1LL*k))
+            while(val>=(1LL*k))    // while the condition is valid, we can take this particular length 
             {
                 ans=min(ans,j-i+1);
                 
                 long long temp=nums[i];
-                for(int l=0;l<32;++l)
+                for(int l=0;l<32;++l)        // Element from the back i.e., i'th element contribution is being taken away
                 {
                     int curr=(1&temp);
                     temp/=2;
