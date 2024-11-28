7013918aea712163d426747018c36daa2f1883af
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2024-11-28 11:05:09 +0530

    Time: 2567 ms (5.25%), Space: 559.4 MB (5.21%) - LeetHub

--- 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
+    // Here dp would not work :
+    // Reason--> once I store the result of (i,j) using path let's say "path" then whenever I again come to (i,j), 
+    // I just used the already saved result. So here is what my code assumes once we get the result for (i,j) 
+    // then it's the optimal. But there is the possibility that a path from a different path that contributed to 
+    // saving the dp result will be coming with less cost(i.e. cost == the number of blockers that needs to remove). 
+    
+    // So, BFS would be a good approach to check from each of the cell with an optimization of whether this particular cells
+    // already has minimum value. Then, No point to call new BFS from this cell.
     int minimumObstacles(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
 
-        vector<vector<int>>dp(m,vector<int>(n,INT_MAX));
+        vector<vector<int>>dp(m,vector<int>(n,INT_MAX));   // Minm obstacles removed to get to a particular cell
 
         queue<vector<int>>q;
         q.push({0,0,0});       // [row,col, minm_obstacles_removed_to_get_to_this_cell]
