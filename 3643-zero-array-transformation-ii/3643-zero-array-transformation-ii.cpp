40f9c2820eff0f290a9d1dfaf6ac149792481555
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2024-12-01 13:06:54 +0530

    Time: 73 ms (45.02%), Space: 369.5 MB (16.01%) - LeetHub

---
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool check(vector<int>nums,vector<vector<int>>&queries,int mid)
+    {
+        int len=nums.size();
+        vector<int>action(len+1);
+
+        for(int i=0;i<mid;++i)
+        {
+            int l=queries[i][0];
+            int r=queries[i][1];
+            int val=queries[i][2];
+
+            action[l]+=val;
+            action[r+1]-=val;
+        }
+
+        int curr=0;
+        for(int i=0;i<len;++i)
+        {
+            curr+=action[i];
+            nums[i]-=curr;
+
+            if(nums[i]>0) return false;
+        }
+
+        return true;
+    }
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int len=nums.size();
+        int m=queries.size();
+
+        int l=0, h=m;
+        int ans=-1;
+
+        while(l<=h)
+        {
+            int mid=l+(h-l)/2;
+            if(check(nums,queries,mid))
+            {
+                ans=mid;
+                h=mid-1;
+            }
+            else l=mid+1;
+        }
+        return ans;
+    }
+};
