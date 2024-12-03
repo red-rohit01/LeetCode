c53f8bdf1713d8da2211c2df499ba86b5679ca8d
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2024-12-03 13:19:37 +0530

    Time: 39 ms (85.02%), Space: 155.7 MB (58.6%) - LeetHub

---
+++ 3648-find-the-maximum-number-of-fruits-collected/3648-find-the-maximum-number-of-fruits-collected.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    int maxCollectedFruits(vector<vector<int>>& fruits) {
+        int ans=0;
+        int n=fruits.size();
+        if(n==2)
+        {
+            ans+=fruits[0][0]+fruits[0][1]+fruits[1][0]+fruits[1][1];
+            return ans;
+        }
+        if(n==3)
+        {
+            ans+=fruits[0][0]+fruits[0][2]+fruits[1][1]+fruits[1][2]+fruits[2][0]+fruits[2][1]+fruits[2][2];
+            return ans;
+        }
+
+        //First Child {0,0}
+        for(int i=0;i<n;++i)
+        {
+            for(int j=0;j<n;++j) 
+            {
+                if(i==j) ans+=fruits[i][j];
+            }
+        }
+        fruits[n-1][n-1]=0;
+
+        vector<vector<int>>dp(n,vector<int>(n));
+        dp[0][n-1]=fruits[0][n-1];
+
+        for(int i=0;i<n;++i)
+        {
+            for(int j=(n-i-1);j<n;++j)
+            {
+                if((i+1)<n && (i+1)<j) dp[i+1][j]=max(dp[i+1][j], dp[i][j]+fruits[i+1][j]);
+
+                if((i+1)<n && (j+1)<n && (i+1)<(j+1)) dp[i+1][j+1]=max(dp[i+1][j+1], dp[i][j]+fruits[i+1][j+1]);
+
+                if((i+1)<n && (i+1)<(j-1)) dp[i+1][j-1]=max(dp[i+1][j-1], dp[i][j]+fruits[i+1][j-1]);
+            }
+        }
+
+        ans+=dp[n-2][n-1];
+
+        for(int i=0;i<n;++i)
+        {
+            for(int j=0;j<n;++j) dp[i][j]=0;
+        }
+
+        dp[n-1][0]=fruits[n-1][0];
+
+        for(int j=0;j<n;++j)
+        {
+            for(int i=(n-j-1);i<n;++i)
+            {
+                if((j+1)<n && (j+1)<i) dp[i][j+1]=max(dp[i][j+1], dp[i][j]+fruits[i][j+1]);
+
+                if((i+1)<n && (j+1)<n && (i+1)>(j+1)) dp[i+1][j+1]=max(dp[i+1][j+1], dp[i][j]+fruits[i+1][j+1]);
+
+                if((i-1)>=0 &&  (i-1)>(j+1)) dp[i-1][j+1]=max(dp[i-1][j+1], dp[i][j]+fruits[i-1][j+1]);
+            }
+        }
+
+        ans+=dp[n-1][n-2];
+        return ans;
+    }
+};
