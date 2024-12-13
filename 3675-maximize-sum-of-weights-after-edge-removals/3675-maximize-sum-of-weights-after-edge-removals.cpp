727a66379da9b18c76b15ea1d8419b2a5b84c96f
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2024-12-13 14:19:51 +0530

    Time: 742 ms (51.53%), Space: 429.8 MB (19.66%) - LeetHub

--- 3675-maximize-sum-of-weights-after-edge-removals/3675-maximize-sum-of-weights-after-edge-removals.cpp
+++ 3675-maximize-sum-of-weights-after-edge-removals/3675-maximize-sum-of-weights-after-edge-removals.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    // Important concept of dp on edges not on nodes.
-    
+    // The idea is to check what would be the maximum if I remove this particular edge or keep this particular edge. 
+    // Dp is checking care of all the different combinations possible.
+    // Also, keep track if the parents had already been removed or not case. 
+
     long long solve(auto &adj, int &k,int st,int p,int is_parent_rem, auto &dp)
     {
         if(dp[st][is_parent_rem]!=-1) return dp[st][is_parent_rem];
 
         long long ans=0;
         int sz=adj[st].size();
-        vector<pair<long long,long long>>candidates;
+        vector<pair<long long,long long>>candidates;   // all the edges possible and the associated values are stored 
         for(auto &x:adj[st])
         {
             int v=x.first;
@@ -22,6 +24,11 @@ public:
             }
         }
 
+        // Let's suppose a node is required to remove '1' edge from it's two total number of edges. Then, I've assumed that 
+        // removing 'first' and keeping the 'second' will give me the maximum value. i.e.,
+        // (r1+d_r2)>(r2+d_r1)
+        // (r1-d_r1)>(r2+d_r2)
+        // It's more preferred as for using both the variable of only one node, I can sort it such that I'm getting the max ans
         sort(candidates.begin(),candidates.end(),[&](auto &a,auto &b)
         {
             long long d_remove1=a.first;
@@ -34,15 +41,15 @@ public:
 
         int len=candidates.size();
 
-        int req_remove=max(0, sz-k-is_parent_rem);
+        int req_remove=max(0, sz-k-is_parent_rem);  // Atleast this no. of edge must be removed from this particular node.
 
         for(int i=0;i<len;++i)
         {
             long long d_remove=candidates[i].first;
             long long remove=candidates[i].second;
-            if(i<req_remove) ans+=remove;
+            if(i<req_remove) ans+=remove;    // while 'cnt' is less than at_least req. I need to remove for sure.
 
-            else ans+=max(d_remove,remove);
+            else ans+=max(d_remove,remove);   // Checking both possibilities and taking the maximum. 
         }
 
         return dp[st][is_parent_rem]=ans;
@@ -60,7 +67,7 @@ public:
             adj[v].push_back({u,w});
         }
 
-        vector<vector<long long>>dp(len+1,vector<long long>(2,-1));
-        return solve(adj,k,0,-1,0,dp);
+        vector<vector<long long>>dp(len+1,vector<long long>(2,-1));  // dp[i][0]--> max value at this node after parent has not been removed
+        return solve(adj,k,0,-1,0,dp);                               // dp[i][1]--> max value at this node after parent has been removed.
     }
 };
