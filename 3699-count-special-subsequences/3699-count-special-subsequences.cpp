e741d4f362322ee20da2a62c90b40ac64cf9c008
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2025-01-11 19:50:05 +0530

    Time: 2441 ms (6.22%), Space: 136.2 MB (25.29%) - LeetHub

---
+++ 3699-count-special-subsequences/3699-count-special-subsequences.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    // The idea is to equate on both sides by (p/q)==(r/s)
+    long long numberOfSubsequences(vector<int>& nums) {
+        int n=nums.size();
+        long long ans=0;
+        unordered_map<string,long long>freq;
+        for(int r=4;r<n;++r)
+        {
+            for(int p=0,q=(r-2);p<(q-1);++p)
+            {
+                int g=__gcd(nums[p],nums[q]);
+                string curr=to_string(nums[p]/g)+"#"+to_string(nums[q]/g);
+                freq[curr]++;
+            }
+
+            for(int s=(r+2);s<n;++s)
+            {
+                int g=__gcd(nums[s],nums[r]);
+                string curr=to_string(nums[s]/g)+"#"+to_string(nums[r]/g);
+                ans+=freq[curr];
+            }
+        }
+        return ans;
+    }
+};
