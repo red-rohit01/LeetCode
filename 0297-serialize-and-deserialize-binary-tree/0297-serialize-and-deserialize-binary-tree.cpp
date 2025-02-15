02a57a52506f48247bba61ae1ec37e8e1b492057
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2025-02-16 00:49:39 +0530

    Time: 26 ms (68.37%), Space: 32.9 MB (51.82%) - LeetHub

---
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
@@ -0,0 +1,72 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+    void serialize_helper(TreeNode* root, string &ans)
+    {
+        if(root==nullptr)
+        {
+            ans+="*";    // using '*' to denote nullptr
+            return;
+        }
+        
+        ans+=to_string(root->val);
+        ans+="#";                    // Using '#' to separate each of the values in case of string
+        serialize_helper(root->left,ans);
+        serialize_helper(root->right,ans);
+    }
+    // Encodes a tree to a single string
+    string serialize(TreeNode *root) {
+        // Your code here
+        string ans="";
+        serialize_helper(root, ans);
+        return ans;
+    }
+
+    // Function to deserialize a list and construct the tree.
+    TreeNode* deserialize_helper(string &arr, int &ind)
+    {
+        if(ind>=arr.size() || arr[ind]=='*')
+        {
+            ind++;
+            return nullptr;
+        }
+        int val=0;
+        bool flag=true;
+        if(arr[ind]=='-')
+        {
+            flag=false;
+            ind++;
+        }
+        while(ind<arr.size() && arr[ind]!='#')
+        {
+            val*=10;
+            val+=(arr[ind]-'0');
+            ind++;
+        }
+        if(flag==false) val*=-1;
+        TreeNode* root=new TreeNode(val);
+        ind++;
+        root->left=deserialize_helper(arr,ind);
+        root->right=deserialize_helper(arr,ind);
+        return root;
+    }
+    // Decodes your encoded data to tree.
+    TreeNode *deserialize(string data) {
+        // Your code here
+        int ind=0;
+        cout<<data<<"\n";
+        return deserialize_helper(data,ind);
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser, deser;
+// TreeNode* ans = deser.deserialize(ser.serialize(root));
