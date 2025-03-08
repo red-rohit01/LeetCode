9cfa335a82ec6d75d4c0e5ae65e1d50d0e965682
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2025-03-08 14:12:02 +0530

    Time: 66 ms (11.04%), Space: 54.1 MB (8.45%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int helper(int i,int n,vector<int>&coins,long long &amount,auto &dp,long long curr)
+    {
+        if(i==n)
+        {
+            if(curr==amount) return 0;
+            
+            return 1e9;
+        }
+
+        if(dp[i][curr]!=-1) return dp[i][curr];
+
+        int not_taken=helper(i+1,n,coins,amount,dp,curr);
+
+        int taken=1e9;
+        if((curr+1LL*coins[i])<=amount)
+        {
+            taken=1+helper(i,n,coins,amount,dp,curr+coins[i]);
+        }
+        return dp[i][curr]=min(taken,not_taken);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        if(amount==0) return 0;
+        int n=coins.size();
+        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
+        
+        long long target=1LL*amount;
+        int ans=helper(0,n,coins,target,dp,0LL);
+        if(ans==1e9) return -1;
+
+        return ans; 
+    }
+};
