ad3297c79bb1479084e233cb51a75bdf35acf028
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2025-03-13 21:19:01 +0530

    Time: 0 ms (100%), Space: 7.8 MB (99.16%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cpp
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    // One of the good variation would be to build the second largest number by rearranging the digits
+    // e.g., [5,9,7,6,6,3,9,6,3,3,6]
+    // largest_no [9,9,7,6,6,6,6,5,3,3,3]
+    // second largest_no --> swap first position from right where they differ   --> [9,9,7,6,6,6,6,3,5,3,3] 
     int maximumSwap(int num) {
         string str=to_string(num);
         vector<int>right_index(10,-1);
