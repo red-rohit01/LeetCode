05aa9a98f1519bc01a2d88e0d908bfb7f129e92e
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2025-03-15 12:35:07 +0530

    Time: 15 ms (92.1%), Space: 17.9 MB (80.55%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,35 +1,48 @@
 class Solution {
 public:
-    int helper(int i,int n,vector<int>&coins,long long &amount,auto &dp,long long curr)
-    {
-        if(i==n)
-        {
-            if(curr==amount) return 0;
+    // int helper(int i,int n,vector<int>&coins,long long &amount,auto &dp,long long curr)
+    // {
+    //     if(i==n)
+    //     {
+    //         if(curr==amount) return 0;
             
-            return 1e9;
-        }
-        if(curr==amount) return 0;
+    //         return 1e9;
+    //     }
+    //     if(curr==amount) return 0;
 
-        if(dp[i][curr]!=-1) return dp[i][curr];
+    //     if(dp[i][curr]!=-1) return dp[i][curr];
 
-        int not_taken=helper(i+1,n,coins,amount,dp,curr);
+    //     int not_taken=helper(i+1,n,coins,amount,dp,curr);
 
-        int taken=1e9;
-        if((curr+1LL*coins[i])<=amount)
-        {
-            taken=1+helper(i,n,coins,amount,dp,curr+coins[i]);
-        }
-        return dp[i][curr]=min(taken,not_taken);
-    }
+    //     int taken=1e9;
+    //     if((curr+1LL*coins[i])<=amount)
+    //     {
+    //         taken=1+helper(i,n,coins,amount,dp,curr+coins[i]);
+    //     }
+    //     return dp[i][curr]=min(taken,not_taken);
+    // }
     int coinChange(vector<int>& coins, int amount) {
-        if(amount==0) return 0;
-        int n=coins.size();
-        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
+        // if(amount==0) return 0;
+        // int n=coins.size();
+        // vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
         
-        long long target=1LL*amount;
-        int ans=helper(0,n,coins,target,dp,0LL);
-        if(ans==1e9) return -1;
+        // long long target=1LL*amount;
+        // int ans=helper(0,n,coins,target,dp,0LL);
+        // if(ans==1e9) return -1;
 
-        return ans; 
+        // return ans; 
+
+        int n=coins.size();
+        vector<int>dp(amount+1,1e9);
+        dp[0]=0;
+        for(int i=0;i<n;++i)
+        {
+            for(int j=coins[i];j<=amount;++j)
+            {
+                dp[j]=min(dp[j],1+dp[j-coins[i]]);
+            }
+        }
+        if(dp[amount]==1e9) return -1;
+        return dp[amount];
     }
 };
