bc8228c9445c811d9d1558a328667f12e427b029
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2025-04-17 20:15:04 +0530

    Time: 4 ms (13.18%), Space: 16.9 MB (6.09%) - LeetHub

--- 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.cpp
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.cpp
@@ -1,15 +1,35 @@
 class Solution {
-public:
-    int countPairs(vector<int>& nums, int k) {
-        int ans=0;
-        int n=nums.size();
-        for(int i=0;i<n;++i)
-        {
-            for(int j=i+1;j<n;++j)
+    public:
+        // TC:O(n*sqrt(k))
+        int countPairs(vector<int>& nums, int k) {
+            int ans=0;
+            unordered_map<int, vector<int>>mpp;
+            for (int i=0;i<nums.size();++i) mpp[nums[i]].push_back(i);
+
+            vector<int>divisors;
+            for (int d=1;(d*d)<=k;++d) 
             {
-                if(nums[i]==nums[j] && ((i*j)%k)==0) ans++;
+                if((k%d)==0) 
+                {
+                    divisors.push_back(d);
+                    if (d!=k/d) divisors.push_back(k / d);
+                }
             }
+            for (auto& [key,indices]:mpp) 
+            {
+                unordered_map<int, int> mpp2;
+                for (int &i:indices) 
+                {
+                    int gcdd=__gcd(i, k);
+                    int need = k / gcdd;
+                    // adding all previous j with j % need == 0
+                    ans+=mpp2[need]; 
+                    for (int &d:divisors) 
+                    {
+                        if(i%d==0) mpp2[d]++;
+                    }
+                }
+            }
+            return ans;
         }
-        return ans;
-    }
-};
+    };
