38c62e828637abf580b4bccc67b2e6a0ee7b6cb7
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2025-05-07 12:09:53 +0530

    Time: 71 ms (7.53%), Space: 42.9 MB (5.06%) - LeetHub

--- 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    // The idea is simple that since At any time It's possible to get the new minimum value for any particular grid and we are allowed
+    // to move in any of the four directions. so, It's a graph based problem for BFS and not only DP
+    // Similar to LC: 2290
     int minTimeToReach(vector<vector<int>>& moveTime) {
         int n=moveTime.size();
         int m=moveTime[0].size();
@@ -19,7 +22,7 @@ public:
             int row=temp[0];
             int col=temp[1];
 
-            if(temp[2]>ans[row][col]) continue;
+            if(temp[2]>ans[row][col]) continue;   // We already got a lower value for this particular grid so, can ignore this 
 
             for(int k=0;k<4;++k) 
             {
