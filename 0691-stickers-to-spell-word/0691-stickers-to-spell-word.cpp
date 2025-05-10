4dac993b163d833a4f810081f5133a1623ab8620
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2025-05-10 12:04:21 +0530

    Time: 96 ms (62.03%), Space: 54.9 MB (34.49%) - LeetHub

---
+++ 0691-stickers-to-spell-word/0691-stickers-to-spell-word.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    // dp[i][j]-->represents that minimum stickers out of first 'i' stickers to build till 'j' mask
+    // The idea is to try out all the possibilities to create the characters of target....which we can check via mask
+    int helper(int ind,int curr,vector<string>&stickers,string &target,vector<vector<int>>&dp,int &mask)
+    {
+        if(curr==mask) return 0;
+        int n=stickers.size();
+        if(ind>=n) return 1e9;
+
+        if(dp[ind][curr]!=-1) return dp[ind][curr];
+
+        int not_take=helper(ind+1,curr,stickers,target,dp,mask);
+        int take=1e9;
+        int prev=curr;
+        int len=target.size();
+        bool flag=false;
+        for(int i=0;i<stickers[ind].size();++i)
+        {
+            for(int l=0;l<len;++l)
+            {
+                if(stickers[ind][i]==target[l])
+                {
+                    if((curr&(1<<l))!=0) continue;   // If already found then no need to make another call
+
+                    else 
+                    {
+                        curr=(curr|(1<<l));
+                        flag=true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if(flag)
+        {
+            take=1+helper(ind,curr,stickers,target,dp,mask);
+        }
+
+        return dp[ind][prev]=min(take,not_take);
+    }
+    int minStickers(vector<string>& stickers, string target) {
+        int n=stickers.size();
+        int len=target.size();
+        int mask=(1<<len)-1;
+        vector<vector<int>>dp(n+1,vector<int>(mask+1,-1));  
+        int ans=helper(0,0,stickers,target,dp,mask);
+        if(ans==1e9) return -1;
+        return ans;
+    }
+};
