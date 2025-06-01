ce7f6e434c8b4f36ec335615ab8c2de108693a25
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2025-06-02 01:53:12 +0530

    Time: 3 ms (80.7%), Space: 8.9 MB (57.31%) - LeetHub

--- 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.cpp
+++ 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.cpp
@@ -21,23 +21,41 @@ public:
     long long distributeCandies(int n, int limit) {
         int total=3*limit;
         if(total<n) return 0;
-        
-        // long long ans=1LL*(n+2)*1LL*(n+1)/2;
-        // long long val1=1LL*(n-limit+1);
-        // long long val2=1LL*(n-2*limit);
-        
-        // if(val2>=2)
-        // {
-        //     long long curr=val2*(val2-1)/2;
-        //     ans+=3* curr;
-        // }
-        
-        // if(val1>=2)
-        // {
-        //     long long curr=val1*(val1-1)/2;
-        //     ans-=3*curr;
-        // }
-        // return ans;
+        // We can solve this in O(1) with the Inclusion-Exclusion Principle.
+        // we subtract the number of distributions in which at least one child receives more than limit candies. 
+        // However, in doing so, we may subtract some distributions multiple times - specifically, the ones where two or more children receive more than limit candies. 
+        // To correct this, we add back the number of cases where at least two children exceed the limit.
+        // But now we've overcounted the distributions where all three children exceed the limit, so we subtract those again.
+
+        // a. three kids with any number of candies= (n+2)C2  
+        // b. one kid with more than limit number of candies= n+2−(limit+1)C2
+        // c. two kids with more than limit number of candies= n+2−2∗(limit+1)C2
+        // d. three kids with more than limit number of candies= n+2−3∗(limit+1)C2
+        // From the principle above, we get ans= 1∗a−3∗b+3∗c−d
+
+        long long ans=1LL*(n+2)*1LL*(n+1)/2;    // Distributing 'n' candies among '3' children is same as placing two dividers. (n-r+1)C(r-1)  here r=3... n+2C2
+        long long b_val=1LL*(n-limit+1);    
+        long long c_val=1LL*(n-2*limit);
+        long long d_val=1LL*(n-3*limit-1);
+
+        if(b_val>=2)
+        {
+            long long val2=b_val*(b_val-1)/2;
+            ans-=3*val2;
+        }
+
+        if(c_val>=2)
+        {
+            long long val3=c_val*(c_val-1)/2;
+            ans+=3*val3;
+        }
+
+        if(d_val>=2)
+        {
+            long long val4=d_val*(d_val-1)/2;
+            ans-=val4;
+        }
+        return ans;
 
         // *DP based implementation*
         // vector<vector<long long>>dp(4,vector<long long>(n+2,-1LL));
@@ -49,17 +67,18 @@ public:
         // If the candies left can be split into A and C then we need to find all possible ways to split them.
         // So we need to count all the possible ways to pick A and C in the range :
         // max(0,(n−B)−limit)≤A≤min(limit,(n−B))
-        long long ans=0LL;
-        long long mn_B=max(0LL,1LL*(n-2*limit));
-        long long mx_B=min(limit,n);
 
-        for(int i=mn_B;i<=mx_B;++i)
-        {
-            // Now we are just finding the possible values for 'A' so the corresponding values for 'C' are always set.
-            long long lower_val=max(0LL,1LL*(n-i-limit));
-            long long upper_val=min(1LL*limit,1LL*(n-i));
-            ans+=(upper_val-lower_val)+1;        
-        }
-        return ans;
+        // long long ans=0LL;
+        // long long mn_B=max(0LL,1LL*(n-2*limit));
+        // long long mx_B=min(limit,n);
+
+        // for(int i=mn_B;i<=mx_B;++i)
+        // {
+        //     // Now we are just finding the possible values for 'A' so the corresponding values for 'C' are always set.
+        //     long long lower_val=max(0LL,1LL*(n-i-limit));
+        //     long long upper_val=min(1LL*limit,1LL*(n-i));
+        //     ans+=(upper_val-lower_val)+1;        
+        // }
+        // return ans;
     }
 };
