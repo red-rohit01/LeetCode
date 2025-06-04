0f5624cfa1eb82f1e1da348bcaf76a39f1352c77
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2025-06-04 19:46:14 +0530

    Time: 11 ms (91.42%), Space: 36.7 MB (81.34%) - LeetHub

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    // Idea is get the maxm char and try to get the maxm substring
+    // Idea is get the highest value char and try to get the maxm substring
     string answerString(string word, int numFriends) {
         if(numFriends==1) return word;
         int n=word.size();
@@ -17,9 +17,9 @@ public:
             if(word[i]==c)
             {
                 int rem_len=(n-i);
-                int curr=numFriends-i;   // Here 'i' number of prev characters can be assigned to each friend
+                int req=numFriends-i; //Here 'i' number of prev characters can be assigned to each friend and then we require (numFriends-i-1) from the rem_len in front
 
-                if(curr>=1) rem_len-=(curr-1);
+                if(req>=1) rem_len-=(req-1);
                 ans=max(ans,word.substr(i,rem_len));
             }
         }
