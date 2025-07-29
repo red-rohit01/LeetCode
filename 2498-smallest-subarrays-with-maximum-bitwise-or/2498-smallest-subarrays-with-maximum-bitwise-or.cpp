2dbc2d2d4138dd3cc03799c3c4fd52a3797e7732
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2025-07-29 22:58:02 +0530

    Time: 51 ms (30.17%), Space: 60.7 MB (54.74%) - LeetHub

---
+++ 2498-smallest-subarrays-with-maximum-bitwise-or/2498-smallest-subarrays-with-maximum-bitwise-or.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    // By looking at how OR works, we can see that bits can only be turned on with 1. 
+    // So, start from the end and keep track of the minimum index that will keep the bit set. 
+    // Then, take the maximum of all the indexes that contain set bits.
+    vector<int> smallestSubarrays(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>res(n,1);
+        vector<int>latest(31);
+        for(int i=n-1;i>=0;--i)
+        {
+            for(int j=0;j<31;++j)      // so, later on we would get the most significant bit.
+            {
+                if(nums[i]&(1<<j)) latest[j]=i;
+
+                res[i]=max(res[i], (latest[j]-i+1));
+            }
+        }
+        return res;
+    }
+};
