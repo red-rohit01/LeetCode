f24e9ce489e8d03ba0ebaa92232748dd3f8e16e7
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2025-08-07 20:39:09 +0530

    Time: 39 ms (87.66%), Space: 155.7 MB (62.99%) - LeetHub

--- 3648-find-the-maximum-number-of-fruits-collected/3648-find-the-maximum-number-of-fruits-collected.cpp
+++ 3648-find-the-maximum-number-of-fruits-collected/3648-find-the-maximum-number-of-fruits-collected.cpp
@@ -14,13 +14,10 @@ public:
             return ans;
         }
 
-        //First Child {0,0}
+        //First Child {0,0}  // To reach the cell {n-1,n-1} It can only traverse in the diagonal direction.
         for(int i=0;i<n;++i)
         {
-            for(int j=0;j<n;++j) 
-            {
-                if(i==j) ans+=fruits[i][j];
-            }
+            ans+=fruits[i][i];
         }
         fruits[n-1][n-1]=0;
 
@@ -29,7 +26,7 @@ public:
 
         for(int i=0;i<n;++i)
         {
-            for(int j=(n-i-1);j<n;++j)
+            for(int j=(n-i-1);j<n;++j)    // Here 'j' is initialized with (n-i-1) as we can allow only this much deviation on 'j' value so that It's ensured to reach (n-1,n-1)
             {
                 if((i+1)<n && (i+1)<j) dp[i+1][j]=max(dp[i+1][j], dp[i][j]+fruits[i+1][j]);
 
@@ -39,7 +36,7 @@ public:
             }
         }
 
-        ans+=dp[n-2][n-1];
+        ans+=dp[n-2][n-1];     // fruits at position {n-1,n-1} was already consumed by the boy moving diagonal
 
         for(int i=0;i<n;++i)
         {
