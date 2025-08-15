ca55453c686f8721729f001b1e5081b29333f24f
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2025-08-15 21:16:29 +0530

    Time: 45 ms (85.95%), Space: 18.8 MB (36.32%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    // We start search the matrix from top right corner. If the target is greater than the value in current position, then the target can 
+    // not be in entire row of current position because the row is sorted, if the target is less than the value in current position, then 
+    // the target can not in the entire column because the column is sorted too.
+    // We can rule out one row or one column each time, so the time complexity is O(m+n).
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        int row=0,col=n-1;    // Intializing the current pointer
+        while(col>=0 && row<m)
+        {
+            if(matrix[row][col]==target) return true;
+
+            else if(matrix[row][col]>target) col--;
+
+            else row++;
+        }
+        return false;
+    }
+};
