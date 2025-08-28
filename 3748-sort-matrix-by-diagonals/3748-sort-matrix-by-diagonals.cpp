22eb496f1350fd42bfab339de74eb1340c067267
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2025-08-28 23:42:17 +0530

    Time: 10 ms (55.76%), Space: 45.2 MB (33.95%) - LeetHub

--- 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.cpp
+++ 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.cpp
@@ -4,23 +4,30 @@ public:
         map<int,vector<int>>mpp;
         int n = grid.size();
 
-        for(int i = 0; i < n ;i++){
-            for(int j = 0 ;j < n;j++){
+        for(int i = 0; i < n ;i++)
+        {
+            for(int j = 0 ;j < n;j++)
+            {
                 mpp[i-j].push_back(grid[i][j]);
             }
         }
 
-        for(auto &it : mpp){
-            if(it.first < 0){
+        for(auto &it : mpp)
+        {
+            if(it.first < 0)
+            {
                 sort(begin(it.second),end(it.second));
             }
-            else{
+            else
+            {
                 sort(begin(it.second),end(it.second),greater<int>());
             }
         }
 
-        for(int i = n-1;i >=0;i--){
-            for(int j = n-1;j >=0 ;j--){
+        for(int i = n-1;i >=0;i--)
+        {
+            for(int j = n-1;j >=0 ;j--)
+            {
                 grid[i][j] = mpp[i-j].back();
                 mpp[i-j].pop_back();
             }
