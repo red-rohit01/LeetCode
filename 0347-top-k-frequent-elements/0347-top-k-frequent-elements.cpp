8d8e347c6f0df8fcaf897f8e55672a5a17118751
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2025-08-31 23:25:45 +0530

    Time: 1 ms (65.28%), Space: 19.8 MB (13.49%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -2,31 +2,59 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         int n=nums.size();
-        if(k==n) return nums;
-        unordered_map<int,int>rk;
-
-        for(int i=0;i<n;++i) rk[nums[i]]++;
-
-        // Initialize a heap with the most frequent elements at the top
-        auto comp=[&rk](int a,int b)
+        unordered_map<int,int>mp;
+        for(int i:nums) mp[i]++;
+        
+        vector<vector<int>>bucket(n+1);
+        for(auto& it:mp)
         {
-            return rk[a]>rk[b];
-        };
-        priority_queue<int,vector<int>,decltype(comp)>pq(comp);
-        // O(N*logk)<O(N*logN)
-        for(auto &x: rk)
-        {
-            pq.push(x.first);
-            if(pq.size()>k) pq.pop();
+            int element=it.first;
+            int freq=it.second;
+            bucket[freq].push_back(element);
         }
 
-        vector<int>ans;
-        for(int i=k-1;i>=0;--i)
+
+        vector<int>res;
+        for(int i=n;i>0;--i)
         {
-            auto x=pq.top();
-            pq.pop();
-            ans.push_back(x);
+            if(bucket[i].size()==0) continue;
+
+            while(bucket[i].size()>0 && k>0)
+            {
+                res.push_back(bucket[i].back());
+                bucket[i].pop_back();
+                k--;
+            }
+            if(k==0) return res;
         }
-        return ans;
+        return res;
+
+        // int n=nums.size();
+        // if(k==n) return nums;
+        // unordered_map<int,int>rk;
+
+        // for(int i=0;i<n;++i) rk[nums[i]]++;
+
+        // // Initialize a heap with the most frequent elements at the top
+        // auto comp=[&rk](int a,int b)
+        // {
+        //     return rk[a]>rk[b];
+        // };
+        // priority_queue<int,vector<int>,decltype(comp)>pq(comp);
+        // // O(N*logk)<O(N*logN)
+        // for(auto &x: rk)
+        // {
+        //     pq.push(x.first);
+        //     if(pq.size()>k) pq.pop();
+        // }
+
+        // vector<int>ans;
+        // for(int i=k-1;i>=0;--i)
+        // {
+        //     auto x=pq.top();
+        //     pq.pop();
+        //     ans.push_back(x);
+        // }
+        // return ans;
     }
 };
