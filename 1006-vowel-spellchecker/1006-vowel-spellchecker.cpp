a419ea440c2ad6268e5650162c4952a451b22a09
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2025-09-14 12:18:05 +0530

    Time: 48 ms (27.22%), Space: 42.1 MB (37.34%) - LeetHub

---
+++ 1006-vowel-spellchecker/1006-vowel-spellchecker.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    // If you use cap[lower] = w; you're replacing the spot in the map with w. If you use insert, STL doesn't replace an existing key.
+    string toLower(string w)
+    {
+        for(auto &c:w) c=tolower(c);
+        return w;
+    }
+    string unVowel(string w)
+    {
+        w=toLower(w);
+        for(auto &c:w)
+        {
+            if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u') c='#';
+        }
+        return w;
+    }
+    vector<string> spellchecker(vector<string>& wordlist, vector<string>& queries) {
+        int n=queries.size();
+        unordered_set<string>words(wordlist.begin(),wordlist.end());
+        unordered_map<string,string>cap,vowel;
+
+        for(auto &w:wordlist)
+        {
+            string lower=toLower(w);
+            string unvowel=unVowel(w);
+
+            cap.insert({lower,w});
+            vowel.insert({unvowel,w});
+        }
+
+        for(auto &w:queries)
+        {
+            if(words.find(w)!=words.end()) continue;
+
+            string lower=toLower(w);
+            string unvowel=unVowel(w);
+
+            if(cap.find(lower)!=cap.end()) w=cap[lower];
+            else if(vowel.find(unvowel)!=vowel.end()) w=vowel[unvowel];
+            else w="";
+        }
+        return queries;
+    }
+};
