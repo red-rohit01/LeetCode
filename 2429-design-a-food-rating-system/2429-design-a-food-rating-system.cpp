526559fb0d19151ece0c4fa93bbd963a56510e67
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2025-09-17 23:01:40 +0530

    Time: 221 ms (10.81%), Space: 170 MB (71.19%) - LeetHub

---
+++ 2429-design-a-food-rating-system/2429-design-a-food-rating-system.cpp
@@ -0,0 +1,52 @@
+class FoodRatings {
+public: 
+    class comp{
+        public:
+        bool operator()(const auto& a,const auto& b)
+        {
+            if(a.first==b.first) return a.second>b.second;
+
+            return a.first<b.first;
+        }
+    };
+    map<string,priority_queue<pair<int,string>, vector<pair<int,string>>, comp>>rk;
+    map<string,string>country;
+    map<string,int>rate;
+    FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
+        rk.clear();
+        int n=cuisines.size();
+        for(int i=0;i<n;i++)
+        {
+            rk[cuisines[i]].push({ratings[i],foods[i]});
+            country[foods[i]]=cuisines[i];
+            rate[foods[i]]=ratings[i];
+        }   
+    }
+    
+    void changeRating(string food, int newRating) {
+        string itr=country[food];
+        rk[itr].push({newRating,food});
+        rate[food]=newRating;
+    }
+    
+    string highestRated(string cuisine) {
+        string ans="";
+        while(rk[cuisine].size()>0)
+        {
+            auto temp=rk[cuisine].top();
+            auto r=temp.first;
+            auto food=temp.second;
+            if(rate[food]==r) return food;
+
+            rk[cuisine].pop();
+        }
+        return ans;
+    }
+};
+
+/**
+ * Your FoodRatings object will be instantiated and called as such:
+ * FoodRatings* obj = new FoodRatings(foods, cuisines, ratings);
+ * obj->changeRating(food,newRating);
+ * string param_2 = obj->highestRated(cuisine);
+ */
