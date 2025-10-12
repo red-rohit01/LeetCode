37a631032759dca32e7b6fcb157029fc5784bc6e
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2025-10-12 14:20:44 +0530

    Time: 279 ms (88.64%), Space: 390 MB (19.7%) - LeetHub

--- 3686-count-beautiful-splits-in-an-array/3686-count-beautiful-splits-in-an-array.cpp
+++ 3686-count-beautiful-splits-in-an-array/3686-count-beautiful-splits-in-an-array.cpp
@@ -1,48 +1,30 @@
-class Solution 
-{
+class Solution {
 public:
-    int beautifulSplits(vector<int>& nums) 
-    {
+    int beautifulSplits(vector<int>& nums) {
         int n=nums.size();
-        if(n<3) return 0;        
-        long mod=1e9+7;
-        long base=50;
+        if(n<3) return 0;
 
-        vector<long long> prefixHash(n+1); // Hash for prefix [0..i)
-        vector<long long> pow(n+1,1); // Store powers of base
-        
-        for(int i=0;i<n;++i) 
+        vector<vector<int>>dp(n+1,vector<int>(n+1)); //Here dp[i][j] represents the length of array starting from index 'i' that matches the array starting from index 'j'
+
+        for(int i=n-1;i>=0;--i)
         {
-            pow[i+1]=(pow[i]*base)%mod;
-            prefixHash[i + 1] = (prefixHash[i] * base + nums[i]) % mod;
-        }
-        int count = 0;
-        for(int i=1;i<n-1;++i) 
-        {
-            for(int j=i+1;j<n;++j) 
+            for(int j=i;j<n;++j)
             {
-                bool valid = false;
-                // Check if nums1 is a prefix of nums2
-                if (isPrefix(prefixHash, 0, i, i, j, mod, pow)) valid=true;
-
-                // Check if nums2 is a prefix of nums3
-                if (!valid && isPrefix(prefixHash, i, j, j, n, mod, pow)) valid=true;
-
-                if (valid) count++;
+                if(nums[i]==nums[j]) dp[i][j]=1+dp[i+1][j+1];
             }
         }
-        return count;
-    }
-private:
-    bool isPrefix(const vector<long long>& hash, int start1, int end1, int start2, int end2, long mod, const vector<long long>& pow) 
-    {
-        int len1=end1-start1;
-        int len2=end2-start2;
-        if(len1>len2) return false;
 
-        long long hash1 = (hash[end1] - (hash[start1] * pow[len1]) % mod + mod) % mod;
-        long long hash2 = (hash[start2 + len1] - (hash[start2] * pow[len1]) % mod + mod) % mod;
-
-        return hash1 == hash2;
+        int ans=0;
+        // num1=={0,i} 
+        // num2=={i+1,j}
+        // num3=={j+1,n-1}
+        for(int i=0;i<n-2;++i)
+        {
+            for(int j=i+1;j<n-1;++j)
+            {
+                if((i+1<=j-i && dp[0][i+1]>=i+1) || (j-i<=n-j && dp[i+1][j+1]>=(j-i))) ans++;
+            }
+        }
+        return ans;
     }
 };
