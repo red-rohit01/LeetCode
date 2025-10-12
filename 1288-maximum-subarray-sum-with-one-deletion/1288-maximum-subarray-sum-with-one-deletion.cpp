159af967f4a1f3febccdf38d383a5a9722f585a2
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2025-10-12 21:17:47 +0530

    Time: 29 ms (16.09%), Space: 38.4 MB (15.32%) - LeetHub

--- 1288-maximum-subarray-sum-with-one-deletion/1288-maximum-subarray-sum-with-one-deletion.cpp
+++ 1288-maximum-subarray-sum-with-one-deletion/1288-maximum-subarray-sum-with-one-deletion.cpp
@@ -22,12 +22,10 @@ public:
         vector<vector<int>>dp(n,vector<int>(2));
         dp[0][0]=arr[0];
         dp[0][1]=0;
-        int total=arr[0];
-        int ans=arr[0];
+        int ans=arr[0];    //As I need to have atleast one element with me
 
         for(int i=1;i<n;++i)
         {
-            total+=arr[i];
             dp[i][0]=max(dp[i-1][0]+arr[i],arr[i]);
             dp[i][1]=max(dp[i-1][1]+arr[i],dp[i-1][0]);
             ans=max({ans,dp[i][0],dp[i][1]});
