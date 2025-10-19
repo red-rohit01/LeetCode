903ca0480a9e2e3d90c56fca9979f0968b616041
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2025-10-19 12:24:33 +0530

    Time: 266 ms (32.3%), Space: 67.3 MB (73.01%) - LeetHub

--- 1747-lexicographically-smallest-string-after-applying-operations/1747-lexicographically-smallest-string-after-applying-operations.cpp
+++ 1747-lexicographically-smallest-string-after-applying-operations/1747-lexicographically-smallest-string-after-applying-operations.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+    // For odd positions, we can have worst case 10 variations using "add a".
+    // For even positions, we can also have worst case 10 variations when b is odd. 
+    // What we do is first we rotate the even positions to the odd positions, and then use "add a". 
+    // So this gives us 10 * 10 variations.
+    // For each of the 10 * 10 variations, we can rotate, if b is odd, worst case each can have n variations, 
+    // where n is the length of s. So finally we can have 10 * 10 * n variations.
     unordered_set<string>vis;
     string ans;
     string rotate(string s, int &b)
