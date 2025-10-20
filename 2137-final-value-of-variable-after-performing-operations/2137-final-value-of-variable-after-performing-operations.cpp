ffdaea7b521f033fcc928253ac6aff7f5cac4609
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2025-10-20 13:16:51 +0530

    Time: 0 ms (100%), Space: 17.4 MB (95.7%) - LeetHub

---
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int ans=0;
+        for(auto &x:operations)
+        {
+            if(x=="--X" || x=="X--") ans--;
+            else ans++;
+        }
+        return ans;
+    }
+};
