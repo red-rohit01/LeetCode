6e6333dd5e1b6088b36dedc08ac9dbe471e850c3
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2025-12-26 13:42:15 +0530

    Time: 12 ms (26.56%), Space: 17.6 MB (55.74%) - LeetHub

--- 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.cpp
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
     int bestClosingTime(string customers) {
-        int y=0,no=0;
+        int y=0;
         int n=customers.size();
-        vector<int>pre(n),suf(n);   //pre[i]--> counts of 'N' till 'i-1'th index and suf[i]--> counts no. of 'Y' from 'i' onward
-        if(customers[0]=='N') no++;
-        for(int i=1;i<n;++i)
-        {
-            pre[i]=no;
-            if(customers[i]=='N') no++;
-        }
-
+        vector<int>suf(n);   // suf[i]--> counts no. of 'Y' from 'i' onward
         for(int i=n-1;i>=0;--i)
         {
            if(customers[i]=='Y') y++;
@@ -19,16 +12,18 @@ public:
         
         int ans=0;
         int mn=n;
+        int pre=0;
         for(int i=0;i<n;++i)
         {
-            int sum=pre[i]+suf[i];
+            int sum=pre+suf[i];
             if(sum<mn)
             {
                 mn=sum;
                 ans=i;
             }
+            if(customers[i]=='N') pre++;
         }
-        if(no<mn) return n;       // To check the closing of shop at n'th hour as this hasn't been checked with for loop
+        if(pre<mn) return n;       // To check the closing of shop at n'th hour as this hasn't been checked with for loop
         return ans;
     }
 };
