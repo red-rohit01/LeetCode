9a92a23a8ab1a2d3ee62ded7abc421f8106f8bcc
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2026-01-11 15:22:33 +0530

    Time: 0 ms (100%), Space: 14.1 MB (79.47%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
@@ -1,98 +1,34 @@
 class Solution {
 public:
-    vector<int>leftMinHelper(vector<int>&arr)
-    {
-        int n=arr.size();
-        vector<int>ans(n,-1);
-        
-        stack<int>st;
-        
-        for(int i=0;i<n;++i)
-        {
-            while(!st.empty() && arr[st.top()]>=arr[i]) st.pop();
-            
-            if(!st.empty()) ans[i]=st.top();
-            
-            st.push(i);
-        }
-        
-        return ans;
-    }
-
-    vector<int>leftMaxHelper(vector<int>&arr)
-    {
-        int n=arr.size();
-        vector<int>ans(n,-1);
-        
-        stack<int>st;
-        
-        for(int i=0;i<n;++i)
-        {
-            while(!st.empty() && arr[st.top()]<=arr[i]) st.pop();
-            
-            if(!st.empty()) ans[i]=st.top();
-            
-            st.push(i);
-        }
-        
-        return ans;
-    }
-
-    vector<int>rightMinHelper(vector<int>&arr)
-    {
-        int n=arr.size();
-        vector<int>ans(n,n);
-        
-        stack<int>st;
-        
-        for(int i=n-1;i>=0;--i)
-        {
-            while(!st.empty() && arr[st.top()]>arr[i]) st.pop();
-            
-            if(!st.empty()) ans[i]=st.top();
-            
-            st.push(i);
-        }
-        
-        return ans;
-    }
-
-    vector<int>rightMaxHelper(vector<int>&arr)
-    {
-        int n=arr.size();
-        vector<int>ans(n,n);
-        
-        stack<int>st;
-        
-        for(int i=n-1;i>=0;--i)
-        {
-            while(!st.empty() && arr[st.top()]<arr[i]) st.pop();
-            
-            if(!st.empty()) ans[i]=st.top();
-            
-            st.push(i);
-        }
-        
-        return ans;
-    }
     long long subArrayRanges(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>leftMin=leftMinHelper(nums);
-        vector<int>leftMax=leftMaxHelper(nums);
-        
-        vector<int>rightMin=rightMinHelper(nums);
-        vector<int>rightMax=rightMaxHelper(nums);
-        
-        
-        long long unfairValue=0;
-        for(int i=0;i<n;++i)
+        int n = nums.size();
+        long long ans=0;
+        stack<int>st;
+        for(int i=0;i<=n;++i)
         {
-            long long mnCnt=1LL*(i-leftMin[i])*1LL*(rightMin[i]-i);
-            long long mxCnt=1LL*(i-leftMax[i])*1LL*(rightMax[i]-i);
-            
-            unfairValue+=mxCnt*1LL*nums[i];
-            unfairValue-=mnCnt*1LL*nums[i];
+            while(!st.empty() && (i==n || nums[i]<=nums[st.top()]))
+            {
+                int ind=st.top();
+                st.pop();
+                int left=-1;
+                if(st.size()>0) left=st.top();
+                ans-=(long long)(1LL*nums[ind]*1LL*(ind-left)*1LL*(i-ind));   // val*left_side_cnt*right_side_cnt
+            }
+            st.push(i);
         }
-        return unfairValue;
+        st.pop();
+        for(int i=0;i<=n;++i)
+        {
+            while(!st.empty() && (i==n || nums[i]>=nums[st.top()]))
+            {
+                int ind=st.top();
+                st.pop();
+                int left=-1;
+                if(st.size()>0) left=st.top();
+                ans+=(long long)(1LL*nums[ind]*1LL*(ind-left)*1LL*(i-ind));
+            }
+            st.push(i);
+        }
+        return ans;
     }
 };
