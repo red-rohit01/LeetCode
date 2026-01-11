7288a0c60db83010a4ca2097efca941222eb3a79
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2026-01-11 15:17:34 +0530

    Time: 3 ms (74.04%), Space: 14.1 MB (72.5%) - LeetHub

---
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        int n = nums.size();
+        long long ans = 0;
+        stack<int>st;
+        for(int i=0;i<=n;++i)
+        {
+            while(!st.empty() && (i==n || nums[i]<=nums[st.top()]))
+            {
+                int ind=st.top();
+                st.pop();
+                int left=-1;
+                if(st.size()>0) left=st.top();
+                ans-=(long long)(1LL*nums[ind]*1LL*(ind-left)*1LL*(i-ind));   // val*left_side_cnt*right_side_cnt
+            }
+            st.push(i);
+        }
+        st.pop();
+        for(int i=0;i<=n;++i)
+        {
+            while(!st.empty() && (i==n || nums[i]>=nums[st.top()]))
+            {
+                int ind=st.top();
+                st.pop();
+                int left=-1;
+                if(st.size()>0) left=st.top();
+                ans+=(long long)(1LL*nums[ind]*1LL*(ind-left)*1LL*(i-ind));
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
