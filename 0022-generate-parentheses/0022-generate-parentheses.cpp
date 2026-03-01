bc552d420bd477cbc2546e6050961d4c07cab899
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2026-03-01 19:33:17 +0530

    Time: 3 ms (70.33%), Space: 15.6 MB (63.82%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,5 +1,19 @@
 class Solution {
 public:
+    // he algorithm explores a pruned recursion tree (never builds invalid sequences).
+    // It generates exactly all valid combinations → optimal.
+    // Total time = (# of valid combinations) × (cost to build each string)
+
+    // Although the recursion tree looks binary, it is heavily pruned by constraints (open < n, open > close).
+    // The algorithm never explores invalid states, so it generates exactly the Catalan number of valid sequences.
+    // Hence time complexity is O(Cₙ · n) and not O(2^{2n}.
+
+    // Cn= 1/(n+1) * 2nCn == (2n)!/(n+1)!xn! 
+    // Total leaves = Cₙ valid sequences
+    // Each sequence length = 2n
+    // Building each string costs O(n)
+
+    // so TC: O(Cn x n)
     void helper(int n,vector<string>&ans,int open,int close,string temp)
     {
         if(open==n && close==n)
