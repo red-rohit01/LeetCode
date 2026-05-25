f4fd4e32c98d145b0cc0fbc7024493fd6268defe
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2026-05-25 23:22:35 +0530

    Time: 8 ms (71.26%), Space: 27.6 MB (31.41%) - LeetHub

--- 2001-jump-game-vii/2001-jump-game-vii.cpp
+++ 2001-jump-game-vii/2001-jump-game-vii.cpp
@@ -6,11 +6,11 @@ public:
         idx.push_back(0);
         for (int i=1;i<n;++i)
         {
-            if (s[i]=='0')
+            if (s[i]=='0')  // I'm just checking if I could reach this particular 'i' with previous farthest reachable index or not
             {
                 for (int j=idx.size()-1;j>=0;j--) //Start searching from the last reachable 0 to decrease time complexity
                 {
-                    if (idx[j]+minJump<=i&&idx[j]+maxJump>=i)
+                    if (idx[j]+minJump<=i && idx[j]+maxJump>=i)
                     {
                         idx.push_back(i);
                         break;
