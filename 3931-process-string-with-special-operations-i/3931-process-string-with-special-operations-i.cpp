059eb75eadbf50abd06487e4ce667f49080ed413
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2026-06-16 22:07:07 +0530

    Time: 11 ms (15.15%), Space: 39.7 MB (87.5%) - LeetHub

---
+++ 3931-process-string-with-special-operations-i/3931-process-string-with-special-operations-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string res;
+        int n=s.size();
+
+        for (int i=0;i<n;++i) 
+        {
+            char ch=s[i];
+
+            if (ch == '*') 
+            {
+                if (res.length() != 0) res.pop_back();
+            } 
+            else if (ch == '#') res+=res;
+            
+            else if (ch == '%') reverse(res.begin(), res.end());
+            
+            else if (ch >= 'a' && ch <= 'z') res.push_back(ch);
+        }
+        return res;
+    }
+};
