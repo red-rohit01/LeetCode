51a0d1d1e3b7973c33731ad997b0d993b8bcb30b
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2026-07-11 14:52:59 +0530

    Time: 7 ms (92.47%), Space: 41.9 MB (18.63%) - LeetHub

--- 4074-count-subarrays-with-majority-element-i/4074-count-subarrays-with-majority-element-i.cpp
+++ 4074-count-subarrays-with-majority-element-i/4074-count-subarrays-with-majority-element-i.cpp
@@ -2,33 +2,43 @@ class Solution {
 public:
     // target --> +1
     // others --> -1
+    // Now in any subarray:
+    // sum = (#target) - (#non-target)
+    // for target to be majority: #target > #non-target == sum>0
+    // we build prefix sums. If pref[i]>pref[j]  then subarray (j+1 ....i) has positive sum.
+    // so the problem becomes "counting how many previous prefix sums are smaller than the current prefix sum"
+    // freq[x]== how many times a prefix sum 'x' appeared.
+
+    // Since the prefix changes by only +1 or −1:
+    // If the prefix increases, only the previous prefixes equal to the old balance become newly smaller, so add their frequency to less.
+    // If the prefix decreases, the previous prefixes equal to the new balance are no longer smaller, so subtract their frequency from less.
     long long countMajoritySubarrays(vector<int>& nums, int target) {
         int n=nums.size();
 
-        vector<int>pre(2*n+1);
-        pre[n]=1;
+        vector<int>freq(2*n+1);   // Here size is (2*n +1) because maximum value can go till 'n' and minimum can '-n'. so, to represent '-n' also we are using the shift value equal to 'n'.
+        int shiftValue=n;
+        freq[n]=1;            // 'n' actually means prefix sum ==0
 
-        int cnt=n;
-        long long presum=0;
+        long long lessCount=0;   // number of previous prefixSums which are smaller than the current prefixSum (i.e., lastSum)
         long long ans=0;
-
+        long long lastSum=0;   // actually storing the prefix sum
         for(int x:nums) 
         {
             if(x==target) 
             {
-                presum+=pre[cnt];
+                lessCount+=freq[lastSum + shiftValue];
 
-                ++cnt;
-                ++pre[cnt];
+                lastSum++;
+                freq[lastSum + shiftValue]++;
             } 
             else 
             {
-                --cnt;
+                lastSum--;
 
-                presum-=pre[cnt];
-                ++pre[cnt];
+                lessCount-=freq[lastSum + shiftValue];
+                ++freq[lastSum + shiftValue];
             }
-            ans+=presum;
+            ans+=lessCount;
         }
         return ans;
     }
