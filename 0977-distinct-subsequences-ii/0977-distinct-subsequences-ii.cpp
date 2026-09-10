ba6f05152369910375ac510e3c32d62d83bab5d4
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2026-09-11 00:05:23 +0530

    Time: 0 ms (100%), Space: 8.7 MB (89.04%) - LeetHub

--- 0977-distinct-subsequences-ii/0977-distinct-subsequences-ii.cpp
+++ 0977-distinct-subsequences-ii/0977-distinct-subsequences-ii.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    // https://leetcode.com/problems/distinct-subsequences-ii/solutions/8506562/trust-me-its-not-hard-optimized-time-spa-yoa5/
     int distinctSubseqII(string s) {
-        int n = s.size();
-        int MOD = 1e9 + 7;
+        int n=s.size();
+        int mod=1e9+7;
 
-        vector<int> countEndWith(26);
+        vector<int>countEndWith(26);     // Number of distinct subsequences currently ending with character 'i'
+        int sum=0;                       // number of distinct non-empty subsequences seen so far.
 
-        int sum = 0;
-
-        for(int i = 0; i < n; i++)
+        for(int i=0;i<n;++i)
         {
-            int idx = s[i] - 'a';
+            int idx=s[i]-'a';
 
             // Create new subsequences and remove duplicates
-            int cur = (1 + sum - countEndWith[idx] + MOD) % MOD;
+            int cur=(1+sum-countEndWith[idx]+mod)%mod; // Every subsequence that was already ending with c will be recreated when we append the new 'idx'.so, subtracted.
 
-            sum = (sum + cur) % MOD;
+            sum=(sum+cur)%mod;
 
             // Update subsequences ending with this character
-            countEndWith[idx] = (countEndWith[idx] + cur) % MOD;
+            countEndWith[idx]=(countEndWith[idx]+cur)%mod;
         }
-
         return sum;
     }
 };
