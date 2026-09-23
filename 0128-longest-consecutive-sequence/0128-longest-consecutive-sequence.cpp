d54db8f1feae137cfd7b231ff05043f4235d52da
Rohit Kumar <113254903+red-rohit01@users.noreply.github.com>
2026-09-24 02:00:03 +0530

    Time: 0 ms (100%), Space: 90.3 MB (13.38%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -13,8 +13,7 @@ public:
     See if n - 1 and n + 1 exist in the map, and if so, it means there is an existing sequence next to n. Variables left and right will 
     be the length of those two sequences, while 0 means there is no sequence and n will be the boundary point later. Store (left + right + 1)
     as the associated value to key n into the map.
-    Use left and right to locate the other end of the sequences to the left and right of n respectively, and replace the value with the new 
-    length.
+    Use left and right to locate the other end of the sequences to the left and right of n respectively, and replace the value with the new length.
     Everything inside the for loop is O(1) so the total time is O(n).
     */
     int longestConsecutive(vector<int>& nums) {
